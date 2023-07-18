a090c4e8e4562fba9188333457527d096705e282
Jack Arthur Harrhy <me@jackharrhy.com>
2023-07-18 13:55:01 -0230

    wii stuff - proper image_table_offset handling

--- wii-stuff/src/main.rs
+++ wii-stuff/src/main.rs
@@ -10,15 +10,26 @@ use nom::{
 const TPL_FILE_IDENTIFIER: [u8; 4] = [0x00, 0x20, 0xAF, 0x30];
 
 fn parse_tpl(input: &[u8]) -> IResult<&[u8], ()> {
+    let mut pos = 0;
     let (input, _) = tag(TPL_FILE_IDENTIFIER)(input)?;
+    pos += 4;
 
     let (input, image_count) = take(4u8)(input)?;
+    pos += 4;
     let image_count = u32::from_be_bytes(image_count.try_into().unwrap());
     println!("image count: {:?}", image_count);
 
     let (input, image_table_offset) = take(4u8)(input)?;
+    pos += 4;
     let image_table_offset = u32::from_be_bytes(image_table_offset.try_into().unwrap());
     println!("image table offset: {:?}", image_table_offset);
+
+    let move_by = image_table_offset - pos;
+    println!("move by: {:?}", move_by);
+
+    let (input, _) = take(move_by)(input)?;
+
+    // for i in 0..image_count {}
 
     Ok((input, ()))
 }
