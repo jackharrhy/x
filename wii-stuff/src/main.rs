fb1d431563d08b146812f9877c729c4d0bdfda96
Jack Arthur Harrhy <me@jackharrhy.com>
2023-07-18 13:37:32 -0230

    fixed the bug, start actually doing stuff!
    - the bug was a fun one, the IResult would return _outside_ the context
      of the main function, therefore the `?` on parse_tpl was causing the
      issue in which bytes would complain about being dropped too early

--- wii-stuff/src/main.rs
+++ wii-stuff/src/main.rs
@@ -2,30 +2,35 @@ use anyhow::Result;
 
 use std::{fs::File, io::Read};
 
-use nom::{bytes::complete::tag, IResult};
+use nom::{
+    bytes::complete::{tag, take},
+    IResult,
+};
 
 const TPL_FILE_IDENTIFIER: [u8; 4] = [0x00, 0x20, 0xAF, 0x30];
 
 fn parse_tpl(input: &[u8]) -> IResult<&[u8], ()> {
     let (input, _) = tag(TPL_FILE_IDENTIFIER)(input)?;
+
+    let (input, image_count) = take(4u8)(input)?;
+    let image_count = u32::from_be_bytes(image_count.try_into().unwrap());
+    println!("image count: {:?}", image_count);
+
+    let (input, image_table_offset) = take(4u8)(input)?;
+    let image_table_offset = u32::from_be_bytes(image_table_offset.try_into().unwrap());
+    println!("image table offset: {:?}", image_table_offset);
+
     Ok((input, ()))
 }
 
-fn read_file_bytes(filename: &str) -> Result<Vec<u8>> {
-    let mut file = File::open(filename)?;
-    let mut bytes = Vec::new();
-    file.read_to_end(&mut bytes)?;
-    Ok(bytes)
-}
-
 fn main() -> Result<()> {
-    println!("Hello, world!");
-
     let mut file = File::open("savebanner.tpl")?;
 
-    let mut bytes = Vec::new();
+    let mut bytes: Vec<u8> = Vec::new();
     file.read_to_end(&mut bytes).expect("read bytes from file");
-    parse_tpl(&bytes)?;
+
+    let bytes = &bytes[..];
+    parse_tpl(bytes).expect("failed to parse tpl");
 
     Ok(())
 }
