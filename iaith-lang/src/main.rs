6a41cb48fee105831672031e7fe16169935e381b
Jack Arthur Harrhy <me@jackharrhy.com>
2023-07-26 16:27:14 -0230

    repl progress

--- iaith-lang/src/main.rs
+++ iaith-lang/src/main.rs
@@ -1,19 +1,64 @@
+use core::panic;
 use std::io::{stdin, stdout, Write};
 
-fn read() -> Result<String, std::io::Error> {
+enum Node {
+    Addition,
+    Subtraction,
+    Integer(i64),
+    List(Vec<Node>),
+}
+
+fn read() -> Result<Vec<Node>, std::io::Error> {
     let mut stdout = stdout();
     let stdin = stdin();
     let mut input = String::new();
 
-    print!("iaith-lang> ");
+    print!("\niaith-lang> ");
     stdout.flush()?;
     stdin.read_line(&mut input)?;
 
-    Ok(input)
+    let mut stack: Vec<Node> = Vec::new();
+    stack.push(Node::List(Vec::new()));
+
+    for c in input.chars() {
+        match c {
+            '+' => match stack.last_mut() {
+                Some(Node::List(list)) => list.push(Node::Addition),
+                _ => panic!("Expected list"),
+            },
+            '-' => match stack.last_mut() {
+                Some(Node::List(list)) => list.push(Node::Subtraction),
+                _ => panic!("Expected list"),
+            },
+            '(' => {
+                stack.push(Node::List(Vec::new()));
+            }
+            ')' => {
+                let node = stack.pop().unwrap();
+                match stack.last_mut() {
+                    Some(Node::List(list)) => list.push(node),
+                    _ => panic!("Expected list"),
+                }
+            }
+            ' ' => (),
+            '\n' => (),
+            _ => panic!("Unexpected character"),
+        }
+    }
+
+    if stack.len() != 1 {
+        panic!("Unclosed parenthesis");
+    }
+
+    match stack.pop().unwrap() {
+        Node::List(list) => Ok(list),
+        _ => panic!("Expected list"),
+    }
 }
 
-fn eval(line: String) -> String {
-    line
+fn eval(nodes: Vec<Node>) -> String {
+    let mut result = String::new();
+    result
 }
 
 fn print(line: String) -> Result<(), std::io::Error> {
